af1a98f2a6c1cd240d97cd85533aaf0d22618234
bestmaa <73009776+bestmaa@users.noreply.github.com>
2020-12-20 19:59:41 +0530

    Add files via upload

---
+++ src/Body.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Myprovider } from './Gdata';
+
+function Body() {
+  const { DataValue } = useContext(Myprovider);
+  return (
+    <div>
+      <div className='container'>
+        <div className='row'>
+          {DataValue
+            ? DataValue.map((d) => {
+                return (
+                  <div
+                    className='col s4 card'
+                    style={{
+                      padding: '20px',
+                      borderRight: '1px solid gray',
+                      borderLeft: '1px solid gray',
+                    }}
+                  >
+                    <div className='col s4'>
+                      <img
+                        src={d.avatar_url}
+                        alt={d.login}
+                        style={{ width: '100%' }}
+                      />
+                    </div>
+                    <div className='col s8'>
+                      <Link to={`/user/${d.login}`}>{d.login}</Link>
+                    </div>
+                  </div>
+                );
+              })
+            : ''}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
